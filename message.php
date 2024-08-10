1e4fa72969e20d2f4c1144ba7e54c66acc904e82
Nipuna Lakruwan <laknipsa@gmail.com>
2024-08-10 23:26:54 +0530

    updated php

--- message.php
+++ message.php
@@ -1,3 +1,25 @@
 <?php
-  echo "This is a message from the message.php file";
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $website = $_POST['website'];
+  $phone = $_POST['phone'];
+  $message = $_POST['message'];
+  
+  if (!empty($email)&&!empty($message)) {
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $receiver = "senarath.lakruwan@gmail.com";
+      $subject = "From: $name <$email>";
+      $body = "Name: $name\nEmail: $email\nWebsite: $website\nPhone: $phone\n\nMessage:\n$message";
+      $sender = "From: $email";
+      if (mail($receiver, $subject, $body, $sender)) {
+        echo "Your message has been sent";
+      }else {
+        echo "Sorry, failed to send your message!";
+      }
+    }else{
+      echo "Please enter a valid email address";
+    }
+  }else {
+    ech
+  }
 ?>
